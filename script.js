0fe9762ae2049b93303f5c58af2dd41412e79acd
raquel <raquelvargas0905@gmail.com>
2024-08-19 18:22:34 -0300

    dando um tempo p respirar

---
+++ script.js
@@ -0,0 +1,12 @@
+function clickMenu(){
+    if (itens.style.display == 'block'){
+        itens.style.display = 'none'
+    } else{
+        itens.style.display = 'block'
+    }
+}
+
+let tt = document.getElementById('tema')
+function trocaTema() {
+    console.alert('Clicou no botão!')
+}
